f3289bb930cba641f05eded13c884cf84bfc3176
akj762662 <80040264+ajimproviser@users.noreply.github.com>
2022-11-11 23:49:24 +0530

    Add files via upload

---
+++ Q_4.java
@@ -0,0 +1,6 @@
+public class Q_4 {
+        public static void main(String[] args)
+        {
+            System.out.println(24);
+        }
+}
---
+++ Q_5.java
@@ -0,0 +1,5 @@
+public class Q_5 {
+        public static void main(String[] args) {
+            System.out.println(10);
+        }
+}
